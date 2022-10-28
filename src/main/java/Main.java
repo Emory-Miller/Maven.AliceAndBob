f1b226dc02ec3aac8815374a61aab1e69e4e245a
Emory <emory@Zipcoder-Emorys-MacBook-Pro.local>
2022-10-27 20:35:34 -0400

    I got this working... I think...

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,31 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import java.util.Scanner;
 public class Main {
-
     public static void main(String[] args ){
+        String name;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter your name:");
+
+        name = sc.nextLine();
+
+        sc.close();
+
+        if ( name.equalsIgnoreCase("Alice")) {
+            System.out.println("Hello Alice!");
+
+        } else if ( name.equalsIgnoreCase("Bob")) {
+
+            System.out.println("Hello Bob!");
+
+        } else {
+
+            System.out.println("Hello!");
+
+        }
 
     }
 }
